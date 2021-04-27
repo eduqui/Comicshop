c83af7a08dfc955701b625bdedd7df71f0750af4
Joseph <jma55cf@gmail.com>
2021-04-27 17:57:24 -0500

    Primer avance  revisar productos

--- src/main/java/com/comicshop/comicshop/controller/productoController.java
+++ src/main/java/com/comicshop/comicshop/controller/productoController.java
@@ -1,5 +1,17 @@
 package com.comicshop.comicshop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.comicshop.comicshop.model.producto;
+
+@Controller
+
 public class productoController {
+
+    
     
 }
